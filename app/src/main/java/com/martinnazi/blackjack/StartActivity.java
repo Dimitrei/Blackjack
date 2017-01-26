b057d98f21ea2fad3ae4d47474c6b61592a1eb30
Dimitrei <dnazi@my.apsu.edu>
2017-01-26 12:18:50 -0600

    Created three layouts and linked up the activities

--- app/src/main/java/com/martinnazi/blackjack/StartActivity.java
+++ app/src/main/java/com/martinnazi/blackjack/StartActivity.java
@@ -1,5 +1,6 @@
 package com.martinnazi.blackjack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,9 +24,12 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.gameButton:
-
+                Intent gIntent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(gIntent);
                 break;
             case R.id.developebutton:
+                Intent dIntent = new Intent(getApplicationContext(), DeveloperActivity.class);
+                startActivity(dIntent);
                 break;
             default:
                 break;
