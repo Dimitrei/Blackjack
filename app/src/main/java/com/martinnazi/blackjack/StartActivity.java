f5810a92ad670a0f5390a18eb3859841576424ad
Dimitrei <dnazi@my.apsu.edu>
2017-01-25 19:43:24 -0600

    Created Developer and Game Activities.

--- app/src/main/java/com/martinnazi/blackjack/StartActivity.java
+++ app/src/main/java/com/martinnazi/blackjack/StartActivity.java
@@ -2,12 +2,33 @@ package com.martinnazi.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button start = (Button) findViewById(R.id.gameButton);
+        Button about = (Button) findViewById(R.id.developebutton);
+
+        start.setOnClickListener(this);
+        about.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gameButton:
+
+                break;
+            case R.id.developebutton:
+                break;
+            default:
+                break;
+        }
     }
 }
